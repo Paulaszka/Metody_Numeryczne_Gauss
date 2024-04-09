7613efcc04a804894b6764b8c73b1f6d2de64f33
Paulaszka <pkropkasz@gmail.com>
2024-04-09 19:42:54 +0200

    works almost perfectly

--- Test1.py
+++ Test1.py
@@ -5,7 +5,7 @@ from functions import *
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        def funkcja(ilosc_rownan,lista_glowna):
+        def funkcja(ilosc_rownan, lista_glowna):
             uklad_sprzeczny = uklad_oznaczony = uklad_nieoznaczony = False
             lista2 = []
             iterator = 0
@@ -14,112 +14,111 @@ class MyTestCase(unittest.TestCase):
                 if iterator < ilosc_rownan - 1:
                     czy_same_zera = False
                     for wiersz in lista_glowna:
-                        czy_same_zera = all(element == 0 for element in wiersz)
+                        czy_same_zera = all(round(element, 10) == 0 for element in wiersz)
                     if czy_same_zera:
                         uklad_nieoznaczony = True
-                        print("Znaleziono nieskonczone rozwiazania")
-                        print(lista_glowna)
                     else:
                         for m in range(iterator, ilosc_rownan):
                             lista2.append(lista_glowna[m][iterator])
                         index1 = lista2.index(max(lista2))
-                        print(lista2)
-                        lista2 = []
-                        print("przed przeminaa")
-                        print(lista_glowna)
-                        for g in range(ilosc_rownan - iterator):
+
+                        for g in range(ilosc_rownan - iterator + 1):
                             lista_glowna[iterator][iterator + g], lista_glowna[iterator + index1][iterator + g] \
                                 = lista_glowna[iterator + index1][iterator + g], lista_glowna[iterator][
                                 iterator + g]
-                        print("po przemianie")
-                        print(lista_glowna)
+
+                        for i in range(ilosc_rownan-2):
+                            if lista_glowna[iterator+1][iterator+1] == 0:
+                                for g in range(ilosc_rownan - iterator):
+                                    lista_glowna[iterator+1][iterator+1 + g], lista_glowna[iterator+2][iterator+1 + g] \
+                                        = lista_glowna[iterator+2][iterator+1 + g], lista_glowna[iterator+1][iterator+1 + g]
+
+                        lista2 = []
                         for a in range(ilosc_rownan):
-                            if lista_glowna[a][a] == 0:
+                            if round(lista_glowna[a][a], 10) == 0:
                                 uklad_sprzeczny = True
                         if not uklad_sprzeczny:
-                            for k in range(ilosc_rownan - 1):
-                                for i in range(k + 1, ilosc_rownan):
-                                    wspolczynnik = lista_glowna[i][k] / lista_glowna[k][k]
-                                    for j in range(k, ilosc_rownan + 1):
-                                        lista_glowna[i][j] -= wspolczynnik * lista_glowna[k][j]
-                                print(lista_glowna)
+                            for i in range(iterator + 1, ilosc_rownan):
+                                wspolczynnik = lista_glowna[i][iterator] / lista_glowna[iterator][iterator]
+                                for j in range(iterator, ilosc_rownan + 1):
+                                    lista_glowna[i][j] -= wspolczynnik * lista_glowna[iterator][j]
                     iterator += 1
                 else:
-                    wyswietl_macierz(lista_glowna)
-                    uklad_oznaczony = True
-                    print("Znaleziono rozwiazanie")
+                    czy_same_zera = False
+                    for wiersz in lista_glowna:
+                        czy_same_zera = all(round(element, 10) == 0 for element in wiersz)
+                    if czy_same_zera:
+                        uklad_nieoznaczony = True
+                        print("Uklad jest nieoznaczony.")
+                    if not uklad_nieoznaczony:
+                        for a in range(ilosc_rownan):
+                            if round(lista_glowna[a][a], 10) == 0:
+                                uklad_sprzeczny = True
+                        if not uklad_sprzeczny:
+                            uklad_oznaczony = True
+
 
             if uklad_sprzeczny:
-                print("Uklad jest sprzeczny")
+                print("Uklad jest sprzeczny.")
 
             if uklad_oznaczony:
+                print("Znaleziono rozwiazanie:")
                 solution = podstawianie_w_tyl(lista_glowna)
-                print("Rozwiązanie:")
                 for i, val in enumerate(solution):
-                    print(f"x{i + 1} = {round(val, 2)}")
+                    if round(val,5) == 0:
+                        print(f"x{i + 1} = {0.0}")
+                    else:
+                        print(f"x{i + 1} = {round(val, 1)}")
 
-        # dataA
-        print("\ndataA:")
-        lista_glowna = np.genfromtxt("./data/dataA.txt", delimiter=',').tolist()
-        funkcja(3, lista_glowna)
-        lista_glowna = []
+        # # dataA
+        # print("\ndataA:")
+        # lista_glowna = np.genfromtxt("./data/dataA.txt", delimiter=',').tolist()
+        # funkcja(3, lista_glowna)
+        #
+        # # dataB
+        # print("\ndataB:")
+        # lista_glowna = np.genfromtxt("./data/dataB.txt", delimiter=',').tolist()
+        # ilosc_rownan1 = 3
+        # funkcja(ilosc_rownan1, lista_glowna)
+        #
+        # # dataC
+        # print("\ndataC:")
+        # lista_glowna = np.genfromtxt("./data/dataC.txt", delimiter=',').tolist()
+        # ilosc_rownan1 = 3
+        # funkcja(ilosc_rownan1, lista_glowna)
+        #
+        # # dataD
+        # print("\ndataD:")
+        # lista_glowna = np.genfromtxt("./data/dataD.txt", delimiter=',').tolist()
+        # funkcja(4, lista_glowna)
+        #
+        # # dataE
+        # print("\ndataE:")
+        # lista_glowna = np.genfromtxt("./data/dataE.txt", delimiter=',').tolist()
+        # funkcja(4, lista_glowna)
+        #
+        # # dataF
+        # print("\ndataF:")
+        # lista_glowna = np.genfromtxt("./data/dataF.txt", delimiter=',').tolist()
+        # funkcja(4, lista_glowna)
+        #
+        # # dataG
+        # print("\ndataG:")
+        # lista_glowna = np.genfromtxt("./data/dataG.txt", delimiter=',').tolist()
+        # funkcja(3, lista_glowna)
+        #
+        # # dataH
+        # print("\ndataH:")
+        # lista_glowna = np.genfromtxt("./data/dataH.txt", delimiter=',').tolist()
+        # funkcja(3, lista_glowna)
+        #
+        # # dataI
+        # print("\ndataI:")
+        # lista_glowna = np.genfromtxt("./data/dataI.txt", delimiter=',').tolist()
+        # funkcja(3, lista_glowna)
+        #
+        # # dataJ
+        # print("\ndataJ:")
+        # lista_glowna = np.genfromtxt("./data/dataJ.txt", delimiter=',').tolist()
+        # funkcja(3, lista_glowna)
 
-        # dataB
-        print("\ndataB:")
-        lista_glowna = np.genfromtxt("./data/dataB.txt", delimiter=',').tolist()
-        ilosc_rownan1 = 3
-        funkcja(ilosc_rownan1, lista_glowna)
-        lista_glowna = []
-
-        # dataC
-        print("\ndataC:")
-        lista_glowna = np.genfromtxt("./data/dataC.txt", delimiter=',').tolist()
-        ilosc_rownan1 = 3
-        funkcja(ilosc_rownan1, lista_glowna)
-        lista_glowna = []
-
-        # dataD
-        print("\ndataD:")
-        lista_glowna = np.genfromtxt("./data/dataD.txt", delimiter=',').tolist()
-        funkcja(4, lista_glowna)
-        lista_glowna = []
-
-        # dataE
-        print("\ndataE:")
-        lista_glowna = np.genfromtxt("./data/dataE.txt", delimiter=',').tolist()
-        funkcja(4, lista_glowna)
-        lista_glowna = []
-
-        # dataF
-        print("\ndataF:")
-        lista_glowna = np.genfromtxt("./data/dataF.txt", delimiter=',').tolist()
-        funkcja(4, lista_glowna)
-        lista_glowna = []
-
-        # dataG
-        print("\ndataG:")
-        lista_glowna = np.genfromtxt("./data/dataG.txt", delimiter=',').tolist()
-        funkcja(3, lista_glowna)
-        lista_glowna = []
-
-        # dataH
-        print("\ndataH:")
-        lista_glowna = np.genfromtxt("./data/dataH.txt", delimiter=',').tolist()
-        funkcja(3, lista_glowna)
-        lista_glowna = []
-
-        # dataI
-        print("\ndataI:")
-        lista_glowna = np.genfromtxt("./data/dataI.txt", delimiter=',').tolist()
-        funkcja(3, lista_glowna)
-        lista_glowna = []
-
-        # dataJ
-        print("\ndataJ:")
-        lista_glowna = np.genfromtxt("./data/dataJ.txt", delimiter=',').tolist()
-        funkcja(3, lista_glowna)
-        lista_glowna = []
-
-
-if __name__ == '__main__':
-    unittest.main()
--- functions.py
+++ functions.py
@@ -1,3 +1,69 @@
+def gauss(ilosc_rownan, lista_glowna):
+    uklad_sprzeczny = uklad_oznaczony = uklad_nieoznaczony = False
+    lista2 = []
+    iterator = 0
+
+    while not uklad_sprzeczny and not uklad_oznaczony and not uklad_nieoznaczony:
+        if iterator < ilosc_rownan - 1:
+            czy_same_zera = False
+            for wiersz in lista_glowna:
+                czy_same_zera = all(round(element, 10) == 0 for element in wiersz)
+            if czy_same_zera:
+                uklad_nieoznaczony = True
+            else:
+                for m in range(iterator, ilosc_rownan):
+                    lista2.append(lista_glowna[m][iterator])
+                index1 = lista2.index(max(lista2))
+
+                for g in range(ilosc_rownan - iterator + 1):
+                    lista_glowna[iterator][iterator + g], lista_glowna[iterator + index1][iterator + g] \
+                        = lista_glowna[iterator + index1][iterator + g], lista_glowna[iterator][
+                        iterator + g]
+
+                for i in range(ilosc_rownan - 2):
+                    if lista_glowna[iterator + 1][iterator + 1] == 0:
+                        for g in range(ilosc_rownan - iterator):
+                            lista_glowna[iterator + 1][iterator + 1 + g], lista_glowna[iterator + 2][iterator + 1 + g] \
+                                = lista_glowna[iterator + 2][iterator + 1 + g], lista_glowna[iterator + 1][
+                                iterator + 1 + g]
+
+                lista2 = []
+                for a in range(ilosc_rownan):
+                    if round(lista_glowna[a][a], 10) == 0:
+                        uklad_sprzeczny = True
+                if not uklad_sprzeczny:
+                    for i in range(iterator + 1, ilosc_rownan):
+                        wspolczynnik = lista_glowna[i][iterator] / lista_glowna[iterator][iterator]
+                        for j in range(iterator, ilosc_rownan + 1):
+                            lista_glowna[i][j] -= wspolczynnik * lista_glowna[iterator][j]
+            iterator += 1
+        else:
+            czy_same_zera = False
+            for wiersz in lista_glowna:
+                czy_same_zera = all(round(element, 10) == 0 for element in wiersz)
+            if czy_same_zera:
+                uklad_nieoznaczony = True
+                print("Uklad jest nieoznaczony.")
+            if not uklad_nieoznaczony:
+                for a in range(ilosc_rownan):
+                    if round(lista_glowna[a][a], 10) == 0:
+                        uklad_sprzeczny = True
+                if not uklad_sprzeczny:
+                    uklad_oznaczony = True
+
+    if uklad_sprzeczny:
+        print("Uklad jest sprzeczny.")
+
+    if uklad_oznaczony:
+        print("Znaleziono rozwiazanie:")
+        solution = podstawianie_w_tyl(lista_glowna)
+        for i, val in enumerate(solution):
+            if round(val, 5) == 0:
+                print(f"x{i + 1} = {0.0}")
+            else:
+                print(f"x{i + 1} = {round(val, 1)}")
+
+
 def wyswietl_macierz(macierz):
     for wiersz1 in macierz:
         for element in wiersz1:
